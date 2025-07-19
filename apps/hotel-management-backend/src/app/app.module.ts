d170e515032731f9e7ae3a3735bbc5ef53c141a4
deryfeb <102779452+deryfeb@users.noreply.github.com>
2025-07-19 16:29:29 +0800

    update(module): user, hotel, booking, auth, customer module

--- apps/hotel-management-backend/src/app/app.module.ts
+++ apps/hotel-management-backend/src/app/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaService } from './prisma/prisma.service';
+import { HotelModule } from './hotel/hotel.module';
+import { UserModule } from './user/user.module';
+import { CustomerModule } from './customer/customer.module';
+import { BookingModule } from './booking/booking.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      playground: true,
+    }),
+    HotelModule,
+    UserModule,
+    CustomerModule,
+    BookingModule,
+    AuthModule,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
