7876f61cb10c38920a4e398e6db8f7746d7a9857
deryfeb <102779452+deryfeb@users.noreply.github.com>
2025-07-21 00:01:44 +0800

    update(hotel): service and test

--- apps/hotel-management-backend/src/app/hotel/hotel.module.ts
+++ apps/hotel-management-backend/src/app/hotel/hotel.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HotelResolver } from './hotel.resolver';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthModule } from '../auth/auth.module';
+import { HotelService } from './hotel.service';
 
 @Module({
   imports: [AuthModule],
-  providers: [HotelResolver, PrismaService],
+  providers: [HotelResolver, PrismaService, HotelService],
 })
 export class HotelModule {}
--- apps/hotel-management-backend/src/app/hotel/hotel.resolver.ts
+++ apps/hotel-management-backend/src/app/hotel/hotel.resolver.ts
@@ -1,86 +1,47 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { Hotel } from './hotel.model';
 import { CreateHotelInput, UpdateHotelInput } from './hotel.dto';
-import { PrismaService } from '../prisma/prisma.service';
+import { HotelService } from './hotel.service';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { Prisma } from 'apps/hotel-management-backend/generated/prisma';
 
 @Resolver(() => Hotel)
 export class HotelResolver {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly hotelService: HotelService) {}
 
   @Query(() => [Hotel], { name: 'hotels' })
   async getHotels(
     @Args('search', { nullable: true }) search?: string,
     @Args('sortBy', { nullable: true }) sortBy?: string,
-    @Args('sortOrder', { nullable: true, defaultValue: 'asc' }) sortOrder?: 'asc' | 'desc',
+    @Args('sortOrder', { nullable: true, defaultValue: 'asc' })
+    sortOrder?: 'asc' | 'desc',
   ) {
-
-    const where: Prisma.HotelWhereInput = search
-      ? {
-          OR: [
-            { name: { contains: search, mode: 'insensitive' } },
-            { location: { contains: search, mode: 'insensitive' } },
-            { description: { contains: search, mode: 'insensitive' } },
-          ],
-        }
-      : {};
-
-    const orderBy: Prisma.HotelOrderByWithRelationInput = sortBy
-      ? { [sortBy]: sortOrder }
-      : { createdAt: 'desc' };
-
-    return this.prisma.hotel.findMany({
-     where,
-     orderBy,
-      include: { bookings: true },
-    });
+    return this.hotelService.findAll(search, sortBy, sortOrder);
   }
 
   @Query(() => Hotel, { name: 'hotel' })
   async getHotel(@Args('id', { type: () => Int }) id: number) {
-    return this.prisma.hotel.findUnique({
-      where: { id },
-      include: { bookings: true },
-    });
+    return this.hotelService.findOne(id);
   }
 
   @Mutation(() => Hotel)
-  //@UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   async createHotel(@Args('input') input: CreateHotelInput) {
-    return this.prisma.hotel.create({
-      data: {
-        name: input.name,
-        location: input.location,
-        description: input.description,
-        image: input.image,
-      },
-    });
+    return this.hotelService.create(input);
   }
 
   @Mutation(() => Hotel)
-  //@UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   async updateHotel(
     @Args('id', { type: () => Int }) id: number,
     @Args('input') input: UpdateHotelInput,
   ) {
-    return this.prisma.hotel.update({
-      where: { id },
-      data: {
-        name: input.name,
-        location: input.location,
-        description: input.description,
-        image: input.image,
-      },
-    });
+    return this.hotelService.update(id, input);
   }
 
   @Mutation(() => Hotel)
-  //@UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   async deleteHotel(@Args('id', { type: () => Int }) id: number) {
-    return this.prisma.hotel.delete({
-      where: { id },
-    });
+    return this.hotelService.remove(id);
   }
 }
