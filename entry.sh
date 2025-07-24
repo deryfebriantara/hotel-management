e8b8776fcd708d895a8bcacf87d313e65f0bb26c
deryfeb <102779452+deryfeb@users.noreply.github.com>
2025-07-24 23:53:31 +0800

    delete(entry): delete entry sh

--- entry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm install --legacy-peer-deps
-npx prisma generate --schema=apps/hotel-management-backend/prisma/schema.prisma
-npx nx build hotel-management-backend --configuration=production
-node dist/apps/hotel-management-backend/main.js
